cd96c916323640c29be2f6b728067fd7c141506b
GitHub Action <action@github.com>
2024-06-23 19:00:49 +0000

    Build and deploy dist files

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,6 +1,7 @@
+import { AxiosRequestConfig } from 'axios';
 export declare function sleep(ms: any): Promise<unknown>;
 export declare function contains(str: any, arr: any): any;
-export declare function fetchWithTimeout(resource: any, options?: any, maxRetries?: number): Promise<import("axios").AxiosResponse<any, any>>;
+export declare function fetchWithTimeout(resource: string, options?: AxiosRequestConfig, maxRetries?: number): Promise<import("axios").AxiosResponse<any, any>>;
 export declare function fetchNumbersFromString(inputString: any): any;
 export declare function parseError(err: any, prefix?: string): {
     status: string;
