adfdbb9a26fcabbfd04a312c5f74b986ec93acf5
SSK <dodieajt@gmail.com>
2024-06-26 08:41:15 +0530

    kuhiu

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const rootPath = process.cwd();
-    console.log(rootPath)
     return 'Hello World!';
   }
 }
