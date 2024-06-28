65c0cbb66189dca2e9a3d6b2fce78fd785cfb3e3
GitHub Action <action@github.com>
2024-06-28 10:01:51 +0000

    Build and deploy dist files

--- dist/utils.js
+++ dist/utils.js
@@ -61,7 +61,8 @@ async function fetchWithTimeout(resource, options = {}, maxRetries = 1) {
                 return responseIPv6;
         }
         catch (error) {
-            if (retryCount < maxRetries) {
+            const errorDetails = parseError(error);
+            if (retryCount < maxRetries && error.code !== 'ERR_NETWORK' && error.code !== "ECONNABORTED" && error.code !== "ETIMEDOUT" && !errorDetails.message.toLowerCase().includes('too many requests') && !axios_1.default.isCancel(error)) {
                 console.log(`Retrying... (${retryCount + 1}/${maxRetries})`);
                 await new Promise(resolve => setTimeout(resolve, 2000));
             }
