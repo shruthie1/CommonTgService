1e55282a7f5937c30e50b170a00463957d55d8cc
SSK <dodieajt@gmail.com>
2024-07-02 20:29:29 +0530

    wjeh

--- src/components/promote-stats/promote-stat.module.ts
+++ src/components/promote-stats/promote-stat.module.ts
@@ -4,10 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PromoteStatService } from './promote-stat.service';
 import { PromoteStatController } from './promote-stat.controller';
 import { PromoteStat, PromoteStatSchema } from './schemas/promote-stat.schema';
+import { ClientModule } from '../clients/client.module';
 
 @Module({
   imports: [initModule,
-    MongooseModule.forFeature([{ name: PromoteStat.name, schema: PromoteStatSchema }])],
+    MongooseModule.forFeature([{ name: PromoteStat.name, schema: PromoteStatSchema }]),
+    ClientModule
+  ],
   controllers: [PromoteStatController],
   providers: [PromoteStatService],
   exports: [PromoteStatService]
