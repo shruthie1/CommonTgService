bb305bef45f4163527bbd2109a7ed9810e3c7d64
GitHub Action <action@github.com>
2024-07-04 17:50:21 +0000

    Build and deploy dist files

--- dist/components/clients/client.service.js
+++ dist/components/clients/client.service.js
@@ -121,8 +121,8 @@ let ClientService = class ClientService {
                 let isArchived = false;
                 if (existingClientUser) {
                     try {
-                        await this.telegramService.createClient(existingClientMobile, false, true);
                         if ((0, utils_1.toBoolean)(setupClientQueryDto.formalities)) {
+                            await this.telegramService.createClient(existingClientMobile, false, true);
                             console.log("Started Formalities");
                             await this.telegramService.updateUsername(existingClientMobile, '');
                             await this.telegramService.updatePrivacyforDeletedAccount(existingClientMobile);
