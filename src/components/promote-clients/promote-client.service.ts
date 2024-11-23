6686a5f17d299e2504d2f6d1333c212de49f672c
SSK <dodieajt@gmail.com>
2024-11-23 13:12:24 +0530

    iijoijo

--- src/components/promote-clients/promote-client.service.ts
+++ src/components/promote-clients/promote-client.service.ts
@@ -297,7 +297,7 @@ export class PromoteClientService {
                             await this.telegramService.updateUsername(document.mobile, '');
                         }
                         if (me.firstName !== "Deleted Account") {
-                            await this.telegramService.updateNameandBio(document.mobile, 'Deleted Account');
+                            await this.telegramService.updateNameandBio(document.mobile, 'Deleted Account', '');
                         }
                         const hasPassword = await cli.hasPassword();
                         if (!hasPassword && badIds.length < 4) {
