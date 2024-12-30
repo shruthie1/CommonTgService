392e898442f1e35874c13f8fb19d7bcf0f90a454
GitHub Action <action@github.com>
2024-12-30 11:24:56 +0000

    Build and deploy dist files

--- dist/components/Telegram/Telegram.service.js
+++ dist/components/Telegram/Telegram.service.js
@@ -246,7 +246,8 @@ let TelegramService = TelegramService_1 = class TelegramService {
     }
     async getChannelInfo(mobile, sendIds = false) {
         const telegramClient = await this.getClient(mobile);
-        return await telegramClient.channelInfo(sendIds);
+        const result = await telegramClient.getDialogs({ limit: 10, archived: false });
+        console.log(result);
     }
     async getAuths(mobile) {
         const telegramClient = await this.getClient(mobile);
