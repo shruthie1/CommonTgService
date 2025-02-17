5f7ab0392ccb1892207859dcb1a23a5955fd9d2e
SSK <dodieajt@gmail.com>
2025-02-17 11:33:50 +0530

    okay

--- upload-build.js
+++ upload-build.js
@@ -18,7 +18,7 @@ async function overwriteFile(branch) {
 
     const url = `https://uptimechecker2.glitch.me/builds`;
     const bodyData = {};
-    bodyData[`cms-nst`] = `https://res.cloudinary.com/${process.env.CL_NAME}/raw/upload/v${result.version}/${result.public_id}`
+    bodyData[`cts`] = `https://res.cloudinary.com/${process.env.CL_NAME}/raw/upload/v${result.version}/${result.public_id}`
 
     const resp = await fetch(url, {
       method: 'PATCH',
