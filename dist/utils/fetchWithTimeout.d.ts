9f1698813407e1c9bbb06b0e8f935228c8d7b7a7
GitHub Action <action@github.com>
2025-02-18 07:37:28 +0000

    Build and deploy dist files

--- dist/utils/fetchWithTimeout.d.ts
+++ dist/utils/fetchWithTimeout.d.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 export declare function fetchWithTimeout(url: string, options?: AxiosRequestConfig & {
     bypassUrl?: string;
+    useIPv6?: boolean;
 }, maxRetries?: number): Promise<AxiosResponse>;
