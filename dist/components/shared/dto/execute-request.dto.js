890e34f72ab52b6e10c2f1fec366f6e8547d400c
GitHub Action <action@github.com>
2025-02-27 13:29:25 +0000

    Build and deploy dist files

--- dist/components/shared/dto/execute-request.dto.js
+++ dist/components/shared/dto/execute-request.dto.js
@@ -9,9 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExecuteRequestDto = void 0;
+exports.ExecuteRequestDto = exports.ResponseType = void 0;
 const class_validator_1 = require("class-validator");
 const swagger_1 = require("@nestjs/swagger");
+const class_transformer_1 = require("class-transformer");
+var ResponseType;
+(function (ResponseType) {
+    ResponseType["JSON"] = "json";
+    ResponseType["TEXT"] = "text";
+    ResponseType["BLOB"] = "blob";
+    ResponseType["ARRAYBUFFER"] = "arraybuffer";
+    ResponseType["STREAM"] = "stream";
+})(ResponseType || (exports.ResponseType = ResponseType = {}));
 class ExecuteRequestDto {
 }
 exports.ExecuteRequestDto = ExecuteRequestDto;
@@ -21,8 +30,8 @@ __decorate([
     __metadata("design:type", String)
 ], ExecuteRequestDto.prototype, "url", void 0);
 __decorate([
-    (0, swagger_1.ApiPropertyOptional)({ enum: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], default: 'GET' }),
-    (0, class_validator_1.IsEnum)(['GET', 'POST', 'PUT', 'DELETE', 'PATCH']),
+    (0, swagger_1.ApiPropertyOptional)({ enum: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'], default: 'GET' }),
+    (0, class_validator_1.IsEnum)(['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS']),
     (0, class_validator_1.IsOptional)(),
     __metadata("design:type", String)
 ], ExecuteRequestDto.prototype, "method", void 0);
@@ -43,4 +52,34 @@ __decorate([
     (0, class_validator_1.IsOptional)(),
     __metadata("design:type", Object)
 ], ExecuteRequestDto.prototype, "params", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ enum: ResponseType, default: ResponseType.JSON }),
+    (0, class_validator_1.IsEnum)(ResponseType),
+    (0, class_validator_1.IsOptional)(),
+    __metadata("design:type", String)
+], ExecuteRequestDto.prototype, "responseType", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ description: 'Request timeout in milliseconds', default: 30000, minimum: 1000, maximum: 300000 }),
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.Min)(1000),
+    (0, class_validator_1.Max)(300000),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_transformer_1.Transform)(({ value }) => parseInt(value)),
+    __metadata("design:type", Number)
+], ExecuteRequestDto.prototype, "timeout", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ description: 'Whether to follow redirects', default: true }),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_transformer_1.Transform)(({ value }) => value === 'true' || value === true),
+    __metadata("design:type", Boolean)
+], ExecuteRequestDto.prototype, "followRedirects", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ description: 'Maximum number of redirects to follow', default: 5, minimum: 0, maximum: 10 }),
+    (0, class_validator_1.IsNumber)(),
+    (0, class_validator_1.Min)(0),
+    (0, class_validator_1.Max)(10),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_transformer_1.Transform)(({ value }) => parseInt(value)),
+    __metadata("design:type", Number)
+], ExecuteRequestDto.prototype, "maxRedirects", void 0);
 //# sourceMappingURL=execute-request.dto.js.map
