25862decd8f91c9d6c547468f71e421c776f041d
GitHub Action <action@github.com>
2025-04-08 11:23:16 +0000

    chore: commit pre-bump changes

--- dist/components/stats/stat.controller.d.ts
+++ dist/components/stats/stat.controller.d.ts
@@ -4,9 +4,9 @@ import { UpdateStatDto } from './update-stat.dto';
 export declare class StatController {
     private readonly statService;
     constructor(statService: StatService);
-    create(createStatDto: CreateStatDto): Promise<import("src").Stat>;
-    findByChatIdAndProfile(chatId: string, profile: string): Promise<import("src").Stat>;
-    update(chatId: string, profile: string, updateStatDto: UpdateStatDto): Promise<import("src").Stat>;
+    create(createStatDto: CreateStatDto): Promise<import("./stat.schema").Stat>;
+    findByChatIdAndProfile(chatId: string, profile: string): Promise<import("./stat.schema").Stat>;
+    update(chatId: string, profile: string, updateStatDto: UpdateStatDto): Promise<import("./stat.schema").Stat>;
     deleteOne(chatId: string, profile: string): Promise<void>;
     deleteAll(): Promise<void>;
 }
