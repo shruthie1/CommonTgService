ae1afb7f52bd0dcb2952691d7de1ddcfdaf87c47
GitHub Action <action@github.com>
2025-04-10 04:54:38 +0000

    chore: commit pre-bump changes

--- dist/middlewares/logger.middleware.js
+++ dist/middlewares/logger.middleware.js
@@ -19,7 +19,7 @@ let LoggerMiddleware = class LoggerMiddleware {
         const { method, originalUrl, baseUrl } = req;
         const userAgent = req.get('user-agent') || '';
         const ip = req.ip;
-        const excludedEndpoints = ['/sendtochannel', '/favicon.', '/tgsignup'];
+        const excludedEndpoints = ['/sendtochannel', '/favicon.', '/tgsignup', '/timestamps'];
         const isExcluded = (url) => excludedEndpoints.some(endpoint => url.startsWith(endpoint));
         if (!isExcluded(originalUrl) && originalUrl !== '/') {
             res.on('finish', () => {
