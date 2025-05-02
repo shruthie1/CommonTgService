1e080b0c06892e6e5b826cbf9ba53c9a96d68158
ssk <dodieajt@gmail.com>
2025-05-02 19:03:56 +0530

    done

--- src/components/Telegram/utils/connection-manager.ts
+++ src/components/Telegram/utils/connection-manager.ts
@@ -122,7 +122,7 @@ class ConnectionManager {
             this.logger.logDebug(mobile, 'Parsing error details...');
             await this.unregisterClient(mobile);
             const errorDetails = parseError(error, mobile, false);
-            await BotConfig.getInstance().sendMessage(ChannelCategory.ACCOUNT_LOGIN_FAILURES, `Login failure: ${errorDetails.message}`);
+            await BotConfig.getInstance().sendMessage(ChannelCategory.ACCOUNT_LOGIN_FAILURES, `${process.env.clientId}::\n${mobile}:${errorDetails.message}`);
             if (contains(errorDetails.message.toLowerCase(), ['expired', 'unregistered', 'deactivated', "revoked", "user_deactivated_ban"])) {
                 this.logger.logOperation(mobile, 'Marking user as expired');
                 await this.usersService.updateByFilter({ $or: [{ tgId: user.tgId }, { mobile: mobile }] }, { expired: true });
