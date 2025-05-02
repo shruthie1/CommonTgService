6e879d832a36b1fd1d529d3e770271997c7ab8f0
GitHub Action <action@github.com>
2025-05-02 12:29:33 +0000

    chore: commit pre-bump changes

--- dist/components/Telegram/Telegram.controller.d.ts
+++ dist/components/Telegram/Telegram.controller.d.ts
@@ -30,7 +30,7 @@ export declare class TelegramController {
         errors: Error[];
     }>;
     searchMessages(mobile: string, queryParams: SearchMessagesDto): Promise<SearchMessagesResponseDto>;
-    getChannelInfo(mobile: string, includeIds?: boolean): Promise<import("src/components/Telegram/types/telegram-responses").ChannelInfo>;
+    getChannelInfo(mobile: string, includeIds?: boolean): Promise<import("./types/telegram-responses").ChannelInfo>;
     forwardMedia(mobile: string, channel?: string, fromChatId?: string): Promise<string>;
     leaveChannel(mobile: string, channel: string): Promise<string>;
     setup2FA(mobile: string): Promise<string>;
@@ -94,7 +94,7 @@ export declare class TelegramController {
     getFilteredMedia(mobile: string, chatId: string, types?: ('photo' | 'video' | 'document' | 'voice')[], startDate?: string, endDate?: string, limit?: number, minId?: number, maxId?: number): Promise<{
         messages: {
             messageId: number;
-            type: "document" | "photo" | "video";
+            type: "document" | "video" | "photo";
             thumb: any;
             caption: string;
             date: number;
