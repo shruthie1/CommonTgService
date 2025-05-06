379cba63b7faa6bbf8e639e48b6d3e81b84f3e4e
GitHub Action <action@github.com>
2025-05-06 03:46:27 +0000

    chore: commit pre-bump changes

--- dist/components/Telegram/TelegramManager.js
+++ dist/components/Telegram/TelegramManager.js
@@ -218,7 +218,7 @@ class TelegramManager {
         }
         for (const bot of bots) {
             await this.deleteChat({ peer: bot, justClear: false });
-            const result = await this.cleanupChat({ chatId: bot });
+            const result = await this.cleanupChat({ chatId: bot, revoke: false });
             console.log("Deleted bot chat:", result);
         }
         await connection_manager_1.connectionManager.unregisterClient(this.phoneNumber);
@@ -1583,6 +1583,7 @@ class TelegramManager {
     async cleanupChat(cleanup) {
         if (!this.client)
             throw new Error('Client not initialized');
+        cleanup.revoke = cleanup.revoke !== undefined ? cleanup.revoke : true;
         const messages = await this.client.getMessages(cleanup.chatId, {
             limit: 100,
             ...(cleanup.beforeDate && {
@@ -1598,7 +1599,7 @@ class TelegramManager {
         });
         if (toDelete.length > 0) {
             await this.client.deleteMessages(cleanup.chatId, toDelete.map(m => m.id), {
-                revoke: true
+                revoke: cleanup.revoke
             });
         }
         return { deletedCount: toDelete.length };
