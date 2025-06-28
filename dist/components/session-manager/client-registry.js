a1a5b12cd9dafbebc4209adb02713b4656a65db6
GitHub Action <action@github.com>
2025-06-28 17:35:41 +0000

    chore: commit pre-bump changes

--- dist/components/session-manager/client-registry.js
+++ dist/components/session-manager/client-registry.js
@@ -150,11 +150,13 @@ let ClientRegistry = ClientRegistry_1 = class ClientRegistry {
                         this.logger.logError(mobile, 'Failed to cleanup temporary client', cleanupError);
                     }
                     finally {
-                        tempClient._destroyed = true;
-                        if (tempClient._sender && typeof tempClient._sender.disconnect === 'function') {
-                            await tempClient._sender.disconnect();
+                        if (tempClient) {
+                            tempClient._destroyed = true;
+                            if (tempClient._sender && typeof tempClient._sender.disconnect === 'function') {
+                                await tempClient._sender.disconnect();
+                            }
+                            tempClient = null;
                         }
-                        tempClient = null;
                     }
                 }
                 this.logger.logOperation(mobile, 'Client disconnected during removal');
