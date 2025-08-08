5046318c778c706ad50f7906932cb4b2a7ad5db8
SSK <sheetysai>
2025-08-08 18:32:54 +0530

    kg

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -6,41 +6,63 @@ import { notifbot } from '../utils/logbots';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    private readonly logger = new Logger('HTTP');
+  private readonly logger = new Logger('HTTP');
 
-    use(req: Request, res: Response, next: NextFunction): void {
-        const { method, originalUrl, baseUrl } = req;
-        const userAgent = req.get('user-agent') || '';
-        const ip = req.ip;
+  use(req: Request, res: Response, next: NextFunction): void {
+    const { method, originalUrl, baseUrl } = req;
+    const userAgent = req.get('user-agent') || '';
+    const ip = req.ip;
 
-        const excludedEndpoints = ['/sendtochannel', '/favicon.', '/tgsignup', '/timestamps'];
-        const isExcluded = (url: string) => excludedEndpoints.some(endpoint => url.startsWith(endpoint));
-        if (!isExcluded(originalUrl) && originalUrl !== '/') {
-            res.on('finish', () => {
-                const { statusCode } = res;
-                const contentLength = res.get('content-length');
-                if (statusCode >= 500) {
-                    fetchWithTimeout(`${notifbot()}&text=${encodeURIComponent(`${process.env.clientId}\nService:${process.env.serviceName}\n\nFailed - ${originalUrl} with ${statusCode}`)}`);
-                    this.logger.error(`${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`);
-                } else if (statusCode >= 400) {
-                    fetchWithTimeout(`${notifbot()}&text=${encodeURIComponent(`${process.env.clientId}\nService:${process.env.serviceName}\n\nFailed - ${originalUrl} with ${statusCode}`)}`);
-                    this.logger.warn(`${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`);
-                } else if (statusCode >= 300) {
-                    this.logger.verbose(`${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`);
-                } else {
-                    this.logger.log(`${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`);
-                }
-            });
-            res.on('error', (error) => {
-                const errorDetails = parseError(error, process.env.clientId);
-                fetchWithTimeout(`${notifbot()}&text=${encodeURIComponent(`Failed :: ${originalUrl} with ${errorDetails.message}`)}`);
-            })
+    const excludedEndpoints = [
+      '/sendtochannel',
+      '/favicon.',
+      '/tgsignup',
+      '/timestamps',
+    ];
+    const isExcluded = (url: string) =>
+      excludedEndpoints.some((endpoint) => url.startsWith(endpoint));
+    if (!isExcluded(originalUrl) && originalUrl !== '/') {
+      res.on('finish', () => {
+        const { statusCode } = res;
+        const contentLength = res.get('content-length');
+        if (statusCode >= 500) {
+          fetchWithTimeout(
+            `${notifbot()}&text=${encodeURIComponent(`${process.env.clientId}\nService:${process.env.serviceName}\n\nFailed - ${originalUrl} with ${statusCode}`)}`,
+          );
+          this.logger.error(
+            `${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`,
+          );
+        } else if (statusCode >= 400) {
+          fetchWithTimeout(
+            `${notifbot()}&text=${encodeURIComponent(`${process.env.clientId}\nService:${process.env.serviceName}\n\nFailed - ${originalUrl} with ${statusCode}`)}`,
+          );
+          this.logger.warn(
+            `${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`,
+          );
+        } else if (statusCode >= 300) {
+          this.logger.verbose(
+            `${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`,
+          );
         } else {
-            if (originalUrl.includes('Video')) {
-                this.logger.log(`Excluded endpoint hit: ${originalUrl} (length: ${originalUrl.length})`);
-            }
+          this.logger.log(
+            `${method} ${originalUrl} ${req.ip} || StatusCode : ${statusCode}`,
+          );
         }
-
-        next();
+      });
+      res.on('error', (error) => {
+        const errorDetails = parseError(error, process.env.clientId);
+        fetchWithTimeout(
+          `${notifbot()}&text=${encodeURIComponent(`${process.env.clientId ? process.env.clientId : process.env.serviceName} Failed :: ${originalUrl} with ${errorDetails.message}`)}`,
+        );
+      });
+    } else {
+      if (originalUrl.includes('Video')) {
+        this.logger.log(
+          `Excluded endpoint hit: ${originalUrl} (length: ${originalUrl.length})`,
+        );
+      }
     }
+
+    next();
+  }
 }
