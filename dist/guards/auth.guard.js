f1167dd4b91d6040c232e8cfcf588032a5452a58
GitHub Action <action@github.com>
2025-08-09 11:38:25 +0000

    chore: commit pre-bump changes

--- dist/guards/auth.guard.js
+++ dist/guards/auth.guard.js
@@ -14,7 +14,8 @@ const logbots_1 = require("../utils/logbots");
 const ALLOWED_IPS = ['31.97.59.2', '148.230.84.50', '13.228.225.19', '18.142.128.26', '54.254.162.138'];
 const ALLOWED_ORIGINS = [
     'https://paidgirl.site',
-    'https://zomcall.netlify.app'
+    'https://zomcall.netlify.app',
+    'https://tgchats.netlify.app'
 ].map(origin => origin.toLowerCase());
 const IGNORE_PATHS = [
     '/',
