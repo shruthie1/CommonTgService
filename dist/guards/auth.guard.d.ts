c0197320b5df3c603e8c707ce03ebb8d694a1694
GitHub Action <action@github.com>
2025-08-09 07:11:39 +0000

    chore: commit pre-bump changes

--- dist/guards/auth.guard.d.ts
+++ dist/guards/auth.guard.d.ts
@@ -3,4 +3,9 @@ export declare class AuthGuard implements CanActivate {
     private readonly logger;
     canActivate(context: ExecutionContext): boolean;
     private isIgnoredPath;
+    private isOriginAllowed;
+    private getHeaderValue;
+    private extractRealClientIP;
+    private extractRealOrigin;
+    private extractProtocol;
 }
