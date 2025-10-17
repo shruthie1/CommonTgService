946cc7c71bd88b69b44b22f97c222811f3c2204c
GitHub Action <action@github.com>
2025-10-17 18:07:24 +0000

    chore: commit pre-bump changes

--- dist/components/active-channels/active-channels.service.js
+++ dist/components/active-channels/active-channels.service.js
@@ -238,6 +238,7 @@ let ActiveChannelsService = class ActiveChannelsService {
                         channelId: { $nin: notIds },
                         participantsCount: { $gt: this.MIN_PARTICIPANTS_COUNT },
                         username: { $ne: null },
+                        deletedCount: { $lte: 30 },
                         canSendMsgs: true,
                         restricted: false,
                         banned: false,
