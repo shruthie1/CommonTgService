623b4cab4deb1ed0b79d7c949c6c4297bfd34980
SSK <dodieajt@gmail.com>
2026-03-29 20:03:48 +0530

    SYNC: feat(webshare-proxy): register WebshareProxyModule in app, add barrel exports

--- src/components/ip-management/ip-management.module.ts
+++ src/components/ip-management/ip-management.module.ts
@@ -1,18 +1,14 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IpManagementController } from './ip-management.controller';
 import { IpManagementService } from './ip-management.service';
 import { ProxyIp, ProxyIpSchema } from './schemas/proxy-ip.schema';
-import { ClientModule } from '../clients/client.module';
-import { PromoteClientModule } from '../promote-clients/promote-client.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: ProxyIp.name, schema: ProxyIpSchema },
         ]),
-        forwardRef(() => ClientModule),
-        forwardRef(() => PromoteClientModule)
     ],
     controllers: [IpManagementController],
     providers: [IpManagementService],
