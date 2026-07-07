47a8d5a4aa9c1c9128cc751f77b983680b4fa964
GitHub Action <action@github.com>
2026-07-07 19:13:39 +0000

    chore: commit pre-bump changes [skip ci]

--- dist/cloudinary.d.ts
+++ dist/cloudinary.d.ts
@@ -1,17 +1,6 @@
-import * as cloudinary from 'cloudinary';
 export declare class CloudinaryService {
     static instance: any;
-    resources: Map<any, any>;
-    constructor();
     static getInstance(_name?: string): Promise<any>;
     downloadAndExtractZip(url: string): Promise<string>;
     getResourcesFromFolder(folderName: any): Promise<string>;
-    createNewFolder(folderName: any): Promise<void>;
-    overwriteFile(): Promise<void>;
-    findAndSaveResources(folderName: string, type: string): Promise<void>;
-    createFolder(folderName: any): Promise<any>;
-    uploadFilesToFolder(folderName: any): Promise<(cloudinary.UploadApiResponse | cloudinary.UploadStream)[]>;
-    printResources(): Promise<void>;
-    get(publicId: any): any;
-    getBuffer(publicId: any): any;
 }
